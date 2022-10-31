f1aa13b00a6c7686a3d6c3f6516e5ae8d464a843
qostua <qostua@gmail.com>
2022-10-31 13:26:48 +0300

    Отрисовал все компоненты (кроме создания нового события) минимум один раз

--- src/main.js
+++ src/main.js
@@ -11,3 +11,29 @@ import {createEventEditingTemplate} from './view/form-editing.js';
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const pageBodyNode = document.querySelector('.page-body');
+const headerInnerNode = pageBodyNode.querySelector('.trip-main');
+const menuWrapNode = headerInnerNode.querySelector('.trip-controls__navigation');
+const filtersWrapNode = headerInnerNode.querySelector('.trip-controls__filters');
+const pageMainNode = pageBodyNode.querySelector('.page-main');
+const tripEventsNode = pageMainNode.querySelector('.trip-events');
+
+renderTemplate(headerInnerNode, createTripInfoTemplate(), 'afterbegin');
+
+const tripInfoNode = pageBodyNode.querySelector('.trip-info');
+
+renderTemplate(tripInfoNode, createTripCostTemplate(), 'beforeend');
+renderTemplate(menuWrapNode, createSiteMenu(), 'beforeend');
+renderTemplate(filtersWrapNode, createTripFilters(), 'beforeend');
+
+renderTemplate(tripEventsNode, createTripSortTemplate(), 'afterbegin');
+renderTemplate(tripEventsNode, createEventsList(), 'beforeend');
+
+const eventsListNode = pageMainNode.querySelector('.trip-events__list');
+
+for (let i = 0; i < 3; i++) {
+  renderTemplate(eventsListNode, createEventPointTemplate(), 'beforeend');
+}
+
+renderTemplate(eventsListNode, createEventEditingTemplate(), 'afterbegin');
