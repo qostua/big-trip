ce8d892df0436a2af3b670e4cef800c0bd000de2
qostua <qostua@gmail.com>
2022-10-31 13:25:29 +0300

    Импортировал все функции создания компонентов

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+import {createTripInfoTemplate} from './view/trip-info.js';
+import {createTripCostTemplate} from './view/trip-cost.js';
+import {createSiteMenu} from './view/menu.js';
+import {createTripFilters} from './view/filters.js';
+import {createTripSortTemplate} from './view/trip-sort.js';
+import {createEventsList} from './view/events-list.js';
+import {createEventPointTemplate} from './view/event-item.js';
+import {createEventAddingTemplate} from './view/form-adding.js';
+import {createEventEditingTemplate} from './view/form-editing.js';
+
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
