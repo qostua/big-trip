7c01e0a5dbd9518c92f9b4464ad0cd83c0cecda5
qostua <qostua@gmail.com>
2022-11-08 14:41:11 +0300

    Компоненты с общей информацией по маршруту теперь стали динамическими

--- src/view/trip-cost.js
+++ src/view/trip-cost.js
@@ -1,5 +1,5 @@
-export const createTripCostTemplate = () => (
+export const createTripCostTemplate = (cost) => (
   `<p class="trip-info__cost">
-    Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    Total: &euro;&nbsp;<span class="trip-info__cost-value">${cost}</span>
   </p>`
 );
--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,9 +1,9 @@
-export const createTripInfoTemplate = () => (
+export const createTripInfoTemplate = (cities, dateRangeString) => (
   `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+      <h1 class="trip-info__title">${cities.join(' &mdash; ')}</h1>
 
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+      <p class="trip-info__dates">${dateRangeString}</p>
     </div>
   </section>`
 );
