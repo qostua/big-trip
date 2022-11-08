bae83798186dd79f3cd5289a7d9f042d22aa9d4d
qostua <qostua@gmail.com>
2022-11-08 14:39:38 +0300

    Добавил модуль с утилитарными функциями

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs';
+
+export const getDayFromDateString = (dateString) => dayjs(dateString).format('MMM D');
+export const getTimeFromDateString = (dateString) => dayjs(dateString).format('HH:mm');
+export const getDateFromDateString = (dateString) => dayjs(dateString).format('DD/MM/YY HH:ss');
+export const getDatesDifferencePerMs = (dateStringFrom, dateStringTo) => dayjs(dateStringTo).diff(dayjs(dateStringFrom));
+export const msToHumanizeTime = (ms) => {
+  const totalMinutes = Math.round(ms / 60000);
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+
+  const hoursString = (hours !== 0) ? `${hours}H` : '';
+  const minutesString = (minutes !== 0) ? `${minutes}M` : '';
+
+  return [hoursString, minutesString].join(' ');
+};
