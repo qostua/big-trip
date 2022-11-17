6246b5e36e4c8d0d4d3b1ef060e48d6803cfa77a
qostua <qostua@gmail.com>
2022-11-17 11:51:38 +0300

    Модуль с заглушкой для пустого списка теперь наследует абстрактный класс

--- src/view/empty-list-plug.js
+++ src/view/empty-list-plug.js
@@ -1,33 +1,17 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
-const LIST_PLUG_TO_FILTERS = {
+const PLUG_TO_FILTERS = {
   'everthing': 'Click New Event to create your first point',
   'past': 'There are no past events now',
   'future': 'There are no future events now',
 };
 
 const createEmptyListPlugTemplate = (activeFilter = 'everthing') => (
-  `<p class="trip-events__msg">${LIST_PLUG_TO_FILTERS[activeFilter]}</p>`
+  `<p class="trip-events__msg">${PLUG_TO_FILTERS[activeFilter]}</p>`
 );
 
-export default class EmptyListPlug {
-  constructor() {
-    this._element = null;
-  }
-
+export default class EmptyListPlug extends AbstractView {
   getTemplate() {
     return createEmptyListPlugTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
