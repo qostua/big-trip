a9e417b7f9cdcfe2b794e71a83c20a368947ac69
qostua <qostua@gmail.com>
2022-11-17 11:48:24 +0300

    Модуль с информацией о поездке теперь принемает на вход массив с датами. Наследует абстрактный класс

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,35 +1,44 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
+import {getDatesDifferencePerMs, getFormatedDateFromDateString} from '../utils/common.js';
 
-const createTripInfoTemplate = (cities, dateRangeString) => (
-  `<section class="trip-main__trip-info  trip-info">
+const createCitiesTitle = (cities) => cities.join(' &mdash; ');
+const createDateRange = (dates) => {
+  const sortDates = dates
+    .slice()
+    .sort((dateA, dateB) => getDatesDifferencePerMs(dateB, dateA));
+
+  const firstDate = sortDates[0];
+  const lastDate = sortDates[sortDates.length - 1];
+
+  const firstMonth = getFormatedDateFromDateString(firstDate, 'MMM');
+  const lastMonth = getFormatedDateFromDateString(lastDate, 'MMM');
+
+  return (firstMonth === lastMonth)
+    ? `${getFormatedDateFromDateString(firstDate, 'MMM D')} - ${getFormatedDateFromDateString(lastDate, 'D')}`
+    : `${getFormatedDateFromDateString(firstDate, 'MMM D')} - ${getFormatedDateFromDateString(lastDate, 'MMM D')}`;
+};
+
+const createTripInfoTemplate = (cities, dates) => {
+  if (cities.length === 0) {
+    return null;
+  }
+  return `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-      <h1 class="trip-info__title">${cities.join(' &mdash; ')}</h1>
+      <h1 class="trip-info__title">${createCitiesTitle(cities)}</h1>
 
-      <p class="trip-info__dates">${dateRangeString}</p>
+      <p class="trip-info__dates">${createDateRange(dates)}</p>
     </div>
-  </section>`
-);
+  </section>`;
+};
 
-export default class TripInfo {
-  constructor(cities, dateRangeString) {
+export default class TripInfo extends AbstractView {
+  constructor(cities, dates) {
+    super();
     this._cities = cities;
-    this._dateRangeString = dateRangeString;
-    this._element = null;
+    this._dates = dates;
   }
 
   getTemplate() {
-    return createTripInfoTemplate(this._cities, this._dateRangeString);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+    return createTripInfoTemplate(this._cities, this._dates);
   }
 }
