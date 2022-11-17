9acc720733470364a82c5829225570bea9356b70
qostua <qostua@gmail.com>
2022-11-17 11:57:02 +0300

    Обработчики теперь навешиваются используя методы класса, в функцию рендера передается компонент целиком

--- src/main.js
+++ src/main.js
@@ -6,21 +6,21 @@ import TripSortView from './view/trip-sort.js';
 import PointsListView from './view/points-list.js';
 import EmptyListPlugView from './view/empty-list-plug.js';
 import PointView from './view/point-item.js';
-import PointEditingView from './view/form-editing.js';
+import PointEditingView from './view/point-item-editing.js';
 
 import {generateEventData} from './mock/point-item.js';
 import {POINT_OFFERS} from './mock/data.js';
 import {generateDescriptionsData} from './mock/destination.js';
 import {generateTripCitiesArray, getTotalCost, getDateRange} from './mock/trip-info.js';
 
-import {render, RenderPosition} from './utils.js';
+import {render, RenderPosition, replace} from './utils/render.js';
 
 const EVENTS_COUNT = 20;
 
-const events = new Array(EVENTS_COUNT).fill().map(() => generateEventData());
+const events = new Array(EVENTS_COUNT).fill(null).map(() => generateEventData());
 const tripCities = generateTripCitiesArray(events);
 const tripCost = getTotalCost(events);
-const tripDateRange = getDateRange(events);
+const tripDates = getDateRange(events);
 const destinations = generateDescriptionsData();
 
 const pageBodyNode = document.querySelector('.page-body');
@@ -30,55 +30,58 @@ const filtersWrapNode = headerInnerNode.querySelector('.trip-controls__filters')
 const pageMainNode = pageBodyNode.querySelector('.page-main');
 const tripEventsNode = pageMainNode.querySelector('.trip-events');
 
-const tripInfoComponent = new TripInfoView(tripCities, tripDateRange);
+if (events.length !== 0) {
+  const tripInfoComponent = new TripInfoView(tripCities, tripDates);
+  render(headerInnerNode, tripInfoComponent, RenderPosition.AFTERBEGIN);
+  render(tripInfoComponent, new TripCostView(tripCost), RenderPosition.BEFOREEND);
+}
 
-render(headerInnerNode, tripInfoComponent.getElement(), RenderPosition.AFTERBEGIN);
-render(tripInfoComponent.getElement(), new TripCostView(tripCost).getElement(), RenderPosition.BEFOREEND);
-render(menuWrapNode, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
-render(filtersWrapNode, new TripFiltersView().getElement(), RenderPosition.BEFOREEND);
+render(menuWrapNode, new SiteMenuView(), RenderPosition.BEFOREEND);
+render(filtersWrapNode, new TripFiltersView(), RenderPosition.BEFOREEND);
 
 const pointsListComponent = new PointsListView();
-render(tripEventsNode, new TripSortView().getElement(), RenderPosition.AFTERBEGIN);
-render(tripEventsNode, pointsListComponent.getElement(), RenderPosition.BEFOREEND);
+render(tripEventsNode, new TripSortView(), RenderPosition.AFTERBEGIN);
+render(tripEventsNode, pointsListComponent, RenderPosition.BEFOREEND);
 
 const renderPoint = (listPoints, point) => {
   const pointComponent = new PointView(point);
   const pointEditingComponent = new PointEditingView(destinations, POINT_OFFERS, point);
 
   const replacePointToForm = () => {
-    listPoints.replaceChild(pointEditingComponent.getElement(), pointComponent.getElement());
+    replace(pointEditingComponent, pointComponent);
   };
   const replaceFormToPoint = () => {
-    listPoints.replaceChild(pointComponent.getElement(), pointEditingComponent.getElement());
+    replace(pointComponent, pointEditingComponent);
   };
 
-  const onEscKeyDown = (event, cb) => {
+  const onEscKeyDown = (event) => {
     if (event.key === 'Escape' || event.key === 'Esc') {
       event.preventDefault();
-      cb();
+      replaceFormToPoint();
       document.removeEventListener('keydown', onEscKeyDown);
     }
   };
 
-  pointComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
+  pointComponent.setRollupBtnClickHandler(() => {
     replacePointToForm();
-    document.addEventListener('keydown', (event) => onEscKeyDown(event, replaceFormToPoint));
+    document.addEventListener('keydown', onEscKeyDown);
   });
-  pointEditingComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
+  pointEditingComponent.setRollupBtnClickHandler(() => {
     replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
-  pointEditingComponent.getElement().querySelector('.event--edit').addEventListener('submit', (event) => {
-    event.preventDefault();
+  pointEditingComponent.setFormSubmitHandler(() => {
     replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
-  render(pointsListComponent.getElement(), pointComponent.getElement(), RenderPosition.BEFOREEND);
+  render(pointsListComponent, pointComponent, RenderPosition.BEFOREEND);
 };
 
 if (events.length === 0) {
-  render(tripEventsNode, new EmptyListPlugView().getElement(), RenderPosition.BEFOREEND);
+  render(tripEventsNode, new EmptyListPlugView(), RenderPosition.BEFOREEND);
 } else {
   for (let i = 1; i < EVENTS_COUNT; i++) {
-    renderPoint(pointsListComponent.getElement(), events[i]);
+    renderPoint(pointsListComponent, events[i]);
   }
 }
