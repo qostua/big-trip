920f5df610fe8965fd341e22c105440c4e911a96
qostua <qostua@gmail.com>
2022-11-22 14:46:41 +0300

    Вынес всю логику для списка с точками в перзентер

--- src/main.js
+++ src/main.js
@@ -2,25 +2,22 @@ import TripInfoView from './view/trip-info.js';
 import TripCostView from './view/trip-cost.js';
 import SiteMenuView from './view/menu.js';
 import TripFiltersView from './view/filters.js';
-import TripSortView from './view/trip-sort.js';
-import PointsListView from './view/points-list.js';
-import EmptyListPlugView from './view/empty-list-plug.js';
-import PointView from './view/point-item.js';
-import PointEditingView from './view/point-item-editing.js';
+
+import TripPresenter from './presenter/trip.js';
 
 import {generateEventData} from './mock/point-item.js';
-import {POINT_OFFERS} from './mock/data.js';
+import {POINT_OFFERS_DATA} from './mock/data.js';
 import {generateDescriptionsData} from './mock/destination.js';
 import {generateTripCitiesArray, getTotalCost, getDateRange} from './mock/trip-info.js';
 
-import {render, RenderPosition, replace} from './utils/render.js';
+import {render, RenderPosition} from './utils/render.js';
 
 const EVENTS_COUNT = 20;
 
-const events = new Array(EVENTS_COUNT).fill(null).map(() => generateEventData());
-const tripCities = generateTripCitiesArray(events);
-const tripCost = getTotalCost(events);
-const tripDates = getDateRange(events);
+const points = new Array(EVENTS_COUNT).fill(null).map(() => generateEventData());
+const tripCities = generateTripCitiesArray(points);
+const tripCost = getTotalCost(points);
+const tripDates = getDateRange(points);
 const destinations = generateDescriptionsData();
 
 const pageBodyNode = document.querySelector('.page-body');
@@ -30,7 +27,7 @@ const filtersWrapNode = headerInnerNode.querySelector('.trip-controls__filters')
 const pageMainNode = pageBodyNode.querySelector('.page-main');
 const tripEventsNode = pageMainNode.querySelector('.trip-events');
 
-if (events.length !== 0) {
+if (points.length !== 0) {
   const tripInfoComponent = new TripInfoView(tripCities, tripDates);
   render(headerInnerNode, tripInfoComponent, RenderPosition.AFTERBEGIN);
   render(tripInfoComponent, new TripCostView(tripCost), RenderPosition.BEFOREEND);
@@ -39,49 +36,6 @@ if (events.length !== 0) {
 render(menuWrapNode, new SiteMenuView(), RenderPosition.BEFOREEND);
 render(filtersWrapNode, new TripFiltersView(), RenderPosition.BEFOREEND);
 
-const pointsListComponent = new PointsListView();
-render(tripEventsNode, new TripSortView(), RenderPosition.AFTERBEGIN);
-render(tripEventsNode, pointsListComponent, RenderPosition.BEFOREEND);
+const tripPresenter = new TripPresenter(tripEventsNode, destinations, POINT_OFFERS_DATA);
 
-const renderPoint = (listPoints, point) => {
-  const pointComponent = new PointView(point);
-  const pointEditingComponent = new PointEditingView(destinations, POINT_OFFERS, point);
-
-  const replacePointToForm = () => {
-    replace(pointEditingComponent, pointComponent);
-  };
-  const replaceFormToPoint = () => {
-    replace(pointComponent, pointEditingComponent);
-  };
-
-  const onEscKeyDown = (event) => {
-    if (event.key === 'Escape' || event.key === 'Esc') {
-      event.preventDefault();
-      replaceFormToPoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
-  };
-
-  pointComponent.setRollupBtnClickHandler(() => {
-    replacePointToForm();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-  pointEditingComponent.setRollupBtnClickHandler(() => {
-    replaceFormToPoint();
-    document.removeEventListener('keydown', onEscKeyDown);
-  });
-  pointEditingComponent.setFormSubmitHandler(() => {
-    replaceFormToPoint();
-    document.removeEventListener('keydown', onEscKeyDown);
-  });
-
-  render(pointsListComponent, pointComponent, RenderPosition.BEFOREEND);
-};
-
-if (events.length === 0) {
-  render(tripEventsNode, new EmptyListPlugView(), RenderPosition.BEFOREEND);
-} else {
-  for (let i = 1; i < EVENTS_COUNT; i++) {
-    renderPoint(pointsListComponent, events[i]);
-  }
-}
+tripPresenter.init(points);
