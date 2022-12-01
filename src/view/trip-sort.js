7df09f56dd2c18fc920963a2f8aca86c51b18b95
qostua <qostua@gmail.com>
2022-12-01 16:31:02 +0300

    Список фильтров теперь генерируется на основе новой структуры данных

--- src/view/trip-sort.js
+++ src/view/trip-sort.js
@@ -1,22 +1,19 @@
 import AbstractView from './abstract.js';
+import {FilterTypes} from '../const.js';
 
-const FILTERS_SORTING = [
-  'day',
-  'event',
-  'time',
-  'price',
-  'offers',
-];
+const SORTING_FILTERS = ['DAY', 'EVENT', 'TIME', 'PRICE', 'OFFERS'];
 
-const createFilterItemTemplate = (filter, isChecked) => (
+const createFilterItemTemplate = (filter, isEnable, isChecked) => (
   `<div class="trip-sort__item  trip-sort__item--${filter}">
     <input
       id="sort-${filter}"
+      data-sort-type="${filter}"
       class="trip-sort__input  visually-hidden"
       type="radio"
       name="trip-sort"
       value="sort-${filter}"
       ${isChecked ? 'checked' : ''}
+      ${isEnable ? '' : 'disabled'}
     >
     <label class="trip-sort__btn" for="sort-${filter}">
       ${filter}
@@ -25,12 +22,12 @@ const createFilterItemTemplate = (filter, isChecked) => (
 );
 
 const createFilterListTemplate = (filters) => filters
-  .map((filter, index) => createFilterItemTemplate(filter, index === 0))
+  .map((filter, index) => createFilterItemTemplate(FilterTypes[filter].name, FilterTypes[filter].mod, index === 0))
   .join('');
 
 const createTripSortTemplate = () => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    ${createFilterListTemplate(FILTERS_SORTING)}
+    ${createFilterListTemplate(SORTING_FILTERS)}
   </form>`
 );
 
