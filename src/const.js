92a49ac96854edcc9a0f5b7ec4b50de399606e0d
qostua <qostua@gmail.com>
2022-12-01 16:06:14 +0300

    Добавил в константы список фильтров

--- src/const.js
+++ src/const.js
@@ -9,3 +9,31 @@ export const POINT_TYPES = [
   'sightseeing',
   'restaurant',
 ];
+
+const FilterMods = {
+  DISABLED: false,
+  ENABLE: true,
+};
+
+export const FilterTypes = {
+  DAY: {
+    name: 'day',
+    mod: FilterMods.ENABLE,
+  },
+  EVENT: {
+    name: 'event',
+    mod: FilterMods.DISABLED,
+  },
+  TIME: {
+    name: 'time',
+    mod: FilterMods.ENABLE,
+  },
+  PRICE: {
+    name: 'price',
+    mod: FilterMods.ENABLE,
+  },
+  OFFERS: {
+    name: 'offers',
+    mod: FilterMods.DISABLED,
+  },
+};
