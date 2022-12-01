bd40bd69dfd655513f5477ac2fb02d0c3aed5bb7
qostua <qostua@gmail.com>
2022-12-01 16:26:12 +0300

    Добавил метод для добавления обработчика на клик по кнопке добавления в избранное

--- src/view/point-item.js
+++ src/view/point-item.js
@@ -65,6 +65,7 @@ export default class Point extends AbstractView {
     this._pointData = pointData;
 
     this._rollupBtnClickHandler = this._rollupBtnClickHandler.bind(this);
+    this._favoriteClickHandler = this._favoriteClickHandler.bind(this);
   }
 
   getTemplate() {
@@ -76,12 +77,22 @@ export default class Point extends AbstractView {
     this._callback.rollupBtnClick();
   }
 
+  _favoriteClickHandler(event) {
+    event.preventDefault();
+    this._callback.favoriteClick();
+  }
+
   setRollupBtnClickHandler(callback) {
     this._callback.rollupBtnClick = callback;
     this
       .getElement()
       .querySelector('.event__rollup-btn')
       .addEventListener('click', this._rollupBtnClickHandler);
+  }
+
+  setFavoriteClickHandler(callback) {
+    this._callback.favoriteClick = callback;
+    this.getElement().querySelector('.event__favorite-btn').addEventListener('click', this._favoriteClickHandler);
   }
 }
 
