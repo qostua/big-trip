674de8f54e171a0e59f2913d729e20d33197147c
qostua <qostua@gmail.com>
2022-12-20 15:12:59 +0300

    Перенес функцию обновления данных из утилсов в модель, добавил методы удаления и добавления точки

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,19 +1,5 @@
 import {getDatesDifferencePerMs} from './common.js';
 
-export const updatePoint = (array, newPoint) => {
-  const index = array.findIndex((point) => point.id === newPoint.id);
-
-  if (index === -1) {
-    return array;
-  }
-
-  return [
-    ...array.slice(0, index),
-    newPoint,
-    ...array.slice(index + 1),
-  ];
-};
-
 export const compareDatePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateFrom, pointA.dateFrom);
 
 export const compareTimePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateTo, pointB.dateFrom) - getDatesDifferencePerMs(pointA.dateTo, pointA.dateFrom);
