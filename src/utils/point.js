f485b2cc13e27613fe49114fa48bd1e95d1e3226
qostua <qostua@gmail.com>
2022-12-23 12:42:33 +0300

    Добавил утилитарные функции для определения просроченности точки маршрута

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,7 +1,10 @@
-import {getDatesDifferencePerMs} from './common.js';
+import {getDatesDifferencePerMs, getTodayDateString} from './common.js';
 
 export const compareDatePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateFrom, pointA.dateFrom);
 
 export const compareTimePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateTo, pointB.dateFrom) - getDatesDifferencePerMs(pointA.dateTo, pointA.dateFrom);
 
 export const comparePricePoints = (pointA, pointB) => pointA.price - pointB.price;
+
+export const isPointPast = (point) => getDatesDifferencePerMs(point.dateFrom, getTodayDateString()) > 0;
+export const isPointFuture = (point) => getDatesDifferencePerMs(point.dateTo, getTodayDateString()) < 0;
