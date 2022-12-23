4080fe5274867deeb40fa3d3b77ea9d73e222db6
qostua <qostua@gmail.com>
2022-12-23 13:10:23 +0300

    Добавил компоненту меню методы добавления обработчика и установки активного пункта

--- src/view/menu.js
+++ src/view/menu.js
@@ -2,7 +2,7 @@ import AbstractView from './abstract.js';
 import {MenuItem} from '../const.js';
 
 const createMenuItemTemplate = (item, isActive) => (
-  `<a class="trip-tabs__btn  ${isActive ? 'trip-tabs__btn--active' : ''}" href="#">
+  `<a class="trip-tabs__btn  ${isActive ? 'trip-tabs__btn--active' : ''}" href="#" data-item="${item.toLowerCase()}">
     ${item}
   </a>`
 );
@@ -18,7 +18,35 @@ const createSiteMenuTemplate = () => (
 );
 
 export default class SiteMenu extends AbstractView {
+  constructor() {
+    super();
+
+    this._menuClickHandler = this._menuClickHandler.bind(this);
+  }
+
   getTemplate() {
     return createSiteMenuTemplate();
+  }
+
+  _menuClickHandler(event) {
+    event.preventDefault();
+    const menuItem = event.target.closest('.trip-tabs__btn');
+    this._callback.menuClickHandler(menuItem.data.item);
+  }
+
+  setMenuClickHandler(callback) {
+    this._callback.menuClickHandler = callback;
+    this.getTemplate().querySelector('.trip-controls__trip-tabs').addEventListener('click', this._menuClickHandler);
+  }
+
+  setMenuItem(menuItem) {
+    this._resetMenu();
+
+    this.getTemplate().querySelector(`.trip-tabs__btn[data-item="${menuItem}"]`).classList.add('trip-tabs__btn--active');
+  }
+
+  _resetMenu() {
+    const menuItems = this.getTemplate().querySelectorAll('.trip-tabs__btn');
+    menuItems.forEach((item) => item.classList.remove('trip-tabs__btn--active'));
   }
 }
