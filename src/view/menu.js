9406514673e089e3340cfddc975d99fb99a26db3
qostua <qostua@gmail.com>
2022-12-24 14:14:34 +0300

    Добавил вьюхе меню метод инициализации

--- src/view/menu.js
+++ src/view/menu.js
@@ -1,5 +1,6 @@
 import AbstractView from './abstract.js';
 import {MenuItem} from '../const.js';
+import {remove, render, RenderPosition, replace} from '../utils/render.js';
 
 const createMenuItemTemplate = (item, isActive) => (
   `<a class="trip-tabs__btn  ${isActive ? 'trip-tabs__btn--active' : ''}" href="#" data-item="${item.toLowerCase()}">
@@ -18,10 +19,28 @@ const createSiteMenuTemplate = () => (
 );
 
 export default class SiteMenu extends AbstractView {
-  constructor() {
+  constructor(menuContainer) {
     super();
 
+    this._menuContainer = menuContainer;
+
+    this._menuComponent = null;
+
     this._menuClickHandler = this._menuClickHandler.bind(this);
+  }
+
+  init() {
+    const prevMenuComponent = this._menuComponent;
+
+    this._menuComponent = this.getElement();
+
+    if (prevMenuComponent === null) {
+      render(this._menuContainer, this._menuComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    replace(this._menuContainer, prevMenuComponent);
+    remove(prevMenuComponent);
   }
 
   getTemplate() {
@@ -31,22 +50,22 @@ export default class SiteMenu extends AbstractView {
   _menuClickHandler(event) {
     event.preventDefault();
     const menuItem = event.target.closest('.trip-tabs__btn');
-    this._callback.menuClickHandler(menuItem.data.item);
+    this._callback.menuClickHandler(menuItem.dataset.item);
   }
 
   setMenuClickHandler(callback) {
     this._callback.menuClickHandler = callback;
-    this.getTemplate().querySelector('.trip-controls__trip-tabs').addEventListener('click', this._menuClickHandler);
+    this.getElement().addEventListener('click', this._menuClickHandler);
   }
 
   setMenuItem(menuItem) {
     this._resetMenu();
 
-    this.getTemplate().querySelector(`.trip-tabs__btn[data-item="${menuItem}"]`).classList.add('trip-tabs__btn--active');
+    this.getElement().querySelector(`.trip-tabs__btn[data-item="${menuItem}"]`).classList.add('trip-tabs__btn--active');
   }
 
   _resetMenu() {
-    const menuItems = this.getTemplate().querySelectorAll('.trip-tabs__btn');
+    const menuItems = this.getElement().querySelectorAll('.trip-tabs__btn');
     menuItems.forEach((item) => item.classList.remove('trip-tabs__btn--active'));
   }
 }
