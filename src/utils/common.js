f72e3adbe1da14079e327c38a2111b10548ffbec
qostua <qostua@gmail.com>
2022-12-26 22:22:02 +0300

    Функция очеловечивания времени из милисекунд теперь переводит в другой формат (с днями)

--- src/utils/common.js
+++ src/utils/common.js
@@ -5,12 +5,19 @@ export const getFormatedDateStringFromDate = (date, format = TimeFormats.DATA) =
 export const getDatesDifferencePerMs = (dateStringFrom, dateStringTo) => dayjs(dateStringTo).diff(dayjs(dateStringFrom));
 export const msToHumanizeTime = (ms) => {
   const totalMinutes = Math.round(ms / 60000);
-  const hours = Math.floor(totalMinutes / 60);
+
+  if (totalMinutes === 0) {
+    return '00M';
+  }
+
+  const days = Math.floor(totalMinutes / 1440);
+  const hours = Math.floor(totalMinutes / 60) % 24;
   const minutes = totalMinutes % 60;
 
-  const hoursString = (hours !== 0) ? `${hours}H` : '';
-  const minutesString = (minutes !== 0) ? `${minutes}M` : '';
+  const daysString = (days !== 0) ? `${String(days).padStart(2, '0')}D` : '';
+  const hoursString = (hours !== 0 || days !== 0) ? `${String(hours).padStart(2, '0')}H` : '';
+  const minutesString = `${String(minutes).padStart(2, '0')}M`;
 
-  return [hoursString, minutesString].join(' ');
+  return [daysString, hoursString, minutesString].join(' ');
 };
 export const getTodayDateString = () => getFormatedDateStringFromDate(dayjs());
