6a660ac6bb01162bbcf3427274cb76d761bdeea8
qostua <qostua@gmail.com>
2023-01-04 21:59:27 +0300

    Рефакторинг

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -11,7 +11,11 @@ export default class Abstract {
   }
 
   getTemplate() {
-    throw new Error('Abstract method not implemented: getTemplate.');
+    if (this instanceof Abstract) {
+      throw new Error('Abstract method not implemented: getTemplate.');
+    }
+
+    return '';
   }
 
   getElement() {
