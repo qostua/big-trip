3e4b6c44f9f6a28a1de6c6a6bd3da70838c345ed
qostua <qostua@gmail.com>
2023-01-04 22:10:29 +0300

    Рефакторинг

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,9 +1,7 @@
-import {getDatesDifferencePerMs, getTodayDateString} from './common.js';
+import {getDatesDifferencePerMs, getTodayDateString} from './time.js';
 
 export const compareDatePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateFrom, pointA.dateFrom);
-
 export const compareTimePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateTo, pointB.dateFrom) - getDatesDifferencePerMs(pointA.dateTo, pointA.dateFrom);
-
 export const comparePricePoints = (pointA, pointB) => pointA.price - pointB.price;
 
 export const isPointPast = (point) => getDatesDifferencePerMs(point.dateFrom, getTodayDateString()) > 0;
--- src/view/filter.js
+++ src/view/filter.js
@@ -1,45 +1,60 @@
 import AbstractView from './abstract.js';
 
-const createFilterItem = (filter, isChecked) => (
+import {FilterType} from '../const.js';
+
+const createFilterItem = (filter, isChecked, isDisabled) => (
   `<div class="trip-filters__filter">
-    <input id="filter-${filter}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter}" ${isChecked ? 'checked' : ''}>
-    <label class="trip-filters__filter-label" for="filter-${filter}">${filter}</label>
+    <input
+      id="filter-${filter.type}"
+      class="trip-filters__filter-input  visually-hidden"
+      type="radio"
+      name="trip-filter"
+      value="${filter.type}" ${isChecked ? 'checked' : ''}
+      ${filter.isEmpty || isDisabled ? 'disabled' : ''}
+    >
+    <label class="trip-filters__filter-label" for="filter-${filter.type}">
+      ${filter.type}
+    </label>
   </div>`
 );
 
-const createFilterList = (filters, currentFilterType) => filters
-  .map((filter) => createFilterItem(filter, filter === currentFilterType))
+const createFilterList = (filters, currentFilterType, isDisabled) => filters
+  .map((filter) => createFilterItem(filter, filter.type === currentFilterType, isDisabled))
   .join('');
 
-const createTripFiltersTemplate = (filters, currentFilterType) => (
-  `<form class="trip-filters" action="#" method="get">
-    ${createFilterList(filters, currentFilterType)}
+const createTripFiltersTemplate = (filters, currentFilterType, isDisabled) => (
+  `<div class="trip-controls__filters">
+    <h2 class="visually-hidden">Filter events</h2>
+    <form class="trip-filters" action="#" method="get">
+      ${createFilterList(filters, currentFilterType, isDisabled)}
 
-    <button class="visually-hidden" type="submit">Accept filter</button>
-  </form>`
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>
+  </div>`
 );
 
 export default class Filter extends AbstractView {
-  constructor(filters, currentFilterType = 'everything') {
+  constructor(filters, currentFilterType = FilterType.EVERYTHING, isDisabled = 'false') {
     super();
 
     this._filters = filters;
     this._currentFilter = currentFilterType;
+    this._isDisabled = isDisabled;
 
     this._filterTypeChangeHandler = this._filterTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createTripFiltersTemplate(this._filters, this._currentFilter);
-  }
-
-  _filterTypeChangeHandler(event) {
-    event.preventDefault();
-    this._callback.filterTypeChange(event.target.value);
+    return createTripFiltersTemplate(this._filters, this._currentFilter, this._isDisabled);
   }
 
   setFilterTypeChangeHandler(callback) {
     this._callback.filterTypeChange = callback;
     this.getElement().addEventListener('change', this._filterTypeChangeHandler);
+  }
+
+  _filterTypeChangeHandler(event) {
+    event.preventDefault();
+    this._callback.filterTypeChange(event.target.value);
   }
 }
