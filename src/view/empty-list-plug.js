661cbd346363c533e90b4959905802d3d1f9f504
qostua <qostua@gmail.com>
2023-01-04 22:08:29 +0300

    Убрал баг с опечаткой названия фильтра

--- src/view/empty-list-plug.js
+++ src/view/empty-list-plug.js
@@ -1,13 +1,15 @@
 import AbstractView from './abstract.js';
 
 const PLUG_TO_FILTERS = {
-  'everthing': 'Click New Event to create your first point',
+  'everything': 'Click New Event to create your first point',
   'past': 'There are no past events now',
   'future': 'There are no future events now',
 };
 
 const createEmptyListPlugTemplate = (activeFilter = 'everthing') => (
-  `<p class="trip-events__msg">${PLUG_TO_FILTERS[activeFilter]}</p>`
+  `<p class="trip-events__msg">
+    ${PLUG_TO_FILTERS[activeFilter]}
+  </p>`
 );
 
 export default class EmptyListPlug extends AbstractView {
