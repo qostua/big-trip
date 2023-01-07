7bbcf1ab3a08be0a55ab3838c86f7ae3cf690efe
qostua <qostua@gmail.com>
2023-01-07 15:20:08 +0300

    Утилитарные функции теперь сортируют от большего к меньшему

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,8 +1,8 @@
 import {getDatesDifferencePerMs, getTodayDateString} from './time.js';
 
 export const compareDatePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateFrom, pointA.dateFrom);
-export const compareTimePoints = (pointA, pointB) => getDatesDifferencePerMs(pointB.dateTo, pointB.dateFrom) - getDatesDifferencePerMs(pointA.dateTo, pointA.dateFrom);
-export const comparePricePoints = (pointA, pointB) => pointA.price - pointB.price;
+export const compareTimePoints = (pointA, pointB) => getDatesDifferencePerMs(pointA.dateTo, pointA.dateFrom) - getDatesDifferencePerMs(pointB.dateTo, pointB.dateFrom);
+export const comparePricePoints = (pointA, pointB) => pointB.price - pointA.price;
 
 export const isPointPast = (point) => getDatesDifferencePerMs(point.dateFrom, getTodayDateString()) > 0;
 export const isPointFuture = (point) => getDatesDifferencePerMs(point.dateTo, getTodayDateString()) < 0;
